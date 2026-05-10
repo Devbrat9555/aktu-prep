623d2749cd4e1ec0c64d8442b308d44ec51983d9
Devbrat <vrat1087@gmail.com>
2026-05-10 22:16:36 +0530

    revert: Remove VoiceNavigator feature

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import { GoalProvider } from './context/GoalProvider.tsx';
 import UserSync from './components/UserSync';
 import InstallPWA from './components/InstallPWA';
 import FloatingAIBuddy from './components/FloatingAIBuddy';
-import VoiceNavigator from './components/VoiceNavigator';
 
 /**
  * @function App
@@ -42,7 +41,6 @@ function App() {
 
                             <InstallPWA />
                             <FloatingAIBuddy />
-                            <VoiceNavigator />
                             {/* Vercel Speed Insights */}
                             <SpeedInsights />
                         </AppProvider>
